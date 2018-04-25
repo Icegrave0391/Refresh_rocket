b60c1980a558330b5765f0630c52ce432647ad9f
Icegrave0391 <849680403@qq.com>
2018-04-25 17:39:08 +0800

    Initial Commit

---
+++ Refresh_rocketTests/Refresh_rocketTests.m
@@ -0,0 +1,39 @@
+//
+//  Refresh_rocketTests.m
+//  Refresh_rocketTests
+//
+//  Created by 张储祺 on 2018/4/25.
+//  Copyright © 2018年 张储祺. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Refresh_rocketTests : XCTestCase
+
+@end
+
+@implementation Refresh_rocketTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
