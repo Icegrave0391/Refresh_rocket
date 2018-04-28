fdc87698486702e1f48f9cdb96a53fbab7fcb1b7
Icegrave0391 <849680403@qq.com>
2018-04-28 22:42:09 +0800

    新加送火箭上天 有贝塞尔曲线和火箭位置重新下来的bug

--- Refresh_rocket/ViewController.m
+++ Refresh_rocket/ViewController.m
@@ -8,11 +8,13 @@
 
 #import "ViewController.h"
 #import "UIView+UIViewPlus.h"
-@interface ViewController ()<UIScrollViewDelegate>
+@interface ViewController ()<UIScrollViewDelegate, CAAnimationDelegate>
 
 @property(nonatomic, strong) CAShapeLayer * shapeLayer ;
 @property(nonatomic, strong) CAShapeLayer * rockerLayer ;
 @property(nonatomic, strong) UIScrollView * scrollView ;
+@property(nonatomic, strong) CAShapeLayer * circleLayer ;
+@property(nonatomic, strong) NSTimer * timer ;
 
 @end
 
@@ -29,9 +31,22 @@
 -(CAShapeLayer *)rockerLayer{
     if(!_rockerLayer){
         _rockerLayer = [CAShapeLayer layer] ;
+        self.rockerLayer.frame = CGRectMake(self.view.center.x - 32, -64 , 64, 64) ;
+        self.rockerLayer.contents = (__bridge id _Nullable)([UIImage imageNamed:@"2-Rocket"].CGImage) ;
+        [self.shapeLayer addSublayer:self.rockerLayer] ;
     }
     return  _rockerLayer ;
 }
+-(CAShapeLayer *)circleLayer{
+    if(!_circleLayer){
+        _circleLayer = [CAShapeLayer layer] ;
+        self.circleLayer.fillColor = [UIColor colorWithRed:100.0/255.0 green:200.0/255.0 blue:200.0/255.0 alpha:1].CGColor ;
+        _circleLayer.frame = self.view.bounds ;
+        [self.scrollView.layer addSublayer:_circleLayer] ;
+    }
+    return _circleLayer ;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     UIScrollView * scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)] ;
@@ -47,53 +62,113 @@
     [self.view.layer insertSublayer:self.shapeLayer atIndex:0 ];
     [self.shapeLayer addSublayer:self.rockerLayer] ;
     
-    [self initRocket] ;
 }
 
 #pragma mark 实现方法
--(void)initRocket{
-    self.rockerLayer.frame = CGRectMake(self.view.center.x - 32, -64 , 64, 64) ;
-    self.rockerLayer.contents = (__bridge id _Nullable)([UIImage imageNamed:@"2-Rocket"].CGImage) ;
-    [self.shapeLayer addSublayer:self.rockerLayer] ;
-}
-
 
 -(void)scrollViewDidScroll:(UIScrollView *)scrollView{
     NSLog(@"scroll") ;
-    CGFloat height = -scrollView.contentOffset.y ;    //scroll down => height < 0
-    UIBezierPath * path = [UIBezierPath bezierPath] ;
-    [path moveToPoint:CGPointMake(0, 0)] ;
-    [path addLineToPoint:CGPointMake(self.view.width, 0)] ;
-    
-    if(height <= 100){
-        [path addLineToPoint:CGPointMake(self.view.width, height)] ;
-        [path addLineToPoint:CGPointMake(0, height)] ;
-        CABasicAnimation * animation = [CABasicAnimation animation] ;
-        animation.keyPath = @"position" ;
-        animation.toValue = [NSValue valueWithCGPoint:CGPointMake(self.view.center.x, height - 32)] ;
-        [self.rockerLayer addAnimation:animation forKey:nil] ;
-        [self.rockerLayer setPosition:CGPointMake(self.view.center.x, height -32)] ;
-        //add animation
+    //初始化手势圆点
+    CGPoint translatedPoint = [scrollView.panGestureRecognizer translationInView:scrollView] ;
+    //向下滑动
+    if(translatedPoint.y > 0){
+        if(self.scrollView.tracking == YES ){
+            CGPoint currentCenterPoint = [self.scrollView.panGestureRecognizer locationInView:self.scrollView] ;
+            
+            UIBezierPath * path = [UIBezierPath bezierPath] ;
+            [path moveToPoint:CGPointMake(currentCenterPoint.x, currentCenterPoint.y - 20)] ;
+            [path addArcWithCenter:currentCenterPoint radius:15.0 startAngle:- M_PI_2 endAngle:M_PI * 1.5 clockwise:YES] ;
+            self.circleLayer.path = path.CGPath ;
+        }
+        //处理火箭抖动
+        if(!self.timer && self.scrollView.panGestureRecognizer.state == UIGestureRecognizerStateChanged){
+            self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5f target:self selector:@selector(stateDetection) userInfo:nil repeats:YES] ;
+            [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes] ;
+        }
         
-    }else{
-        //add animation
-        [path addLineToPoint:CGPointMake(self.view.width, 100)] ;
-        [path addQuadCurveToPoint:CGPointMake(0, 100) controlPoint:CGPointMake(self.view.center.x, height)] ;
-        CABasicAnimation * animation = [CABasicAnimation animation] ;
-        animation.keyPath = @"position" ;
-        animation.toValue = [NSValue valueWithCGPoint:CGPointMake(self.view.center.x, height - 32)] ;
-        [self.rockerLayer addAnimation:animation forKey:nil] ;
-        [self.rockerLayer setPosition:CGPointMake(self.view.center.x, height -32)] ;
+        
+        //展示shapeLayer
+        
+        CGFloat height = -scrollView.contentOffset.y ;    //scroll down => height < 0
+        UIBezierPath * path = [UIBezierPath bezierPath] ;
+        [path moveToPoint:CGPointMake(0, 0)] ;
+        [path addLineToPoint:CGPointMake(self.view.width, 0)] ;
+        
+        if(height <= 100){
+            [path addLineToPoint:CGPointMake(self.view.width, height)] ;
+            [path addLineToPoint:CGPointMake(0, height)] ;
+            CABasicAnimation * animation = [CABasicAnimation animation] ;
+            //火箭位置
+            animation.keyPath = @"position" ;
+            animation.duration = 0.1f ;
+            animation.toValue = [NSValue valueWithCGPoint:CGPointMake(self.view.center.x, height - 32)] ;
+            [self.rockerLayer addAnimation:animation forKey:nil] ;
+            [self.rockerLayer setPosition:CGPointMake(self.view.center.x, height -32)] ;
+            //add animation
+            
+        }else{
+            //add animation
+            [path addLineToPoint:CGPointMake(self.view.width, 100)] ;
+            [path addQuadCurveToPoint:CGPointMake(0, 100) controlPoint:CGPointMake(self.view.center.x, height)] ;
+            
+            CGPoint P0 = CGPointMake(self.view.width, 100) ;
+            CGPoint P2 = CGPointMake(0, 100) ;
+            CGPoint P1 = CGPointMake(self.view.center.x, height) ;
+            //确定曲线最低点
+            CGPoint P = CGPointMake(0.25*(P0.x+P2.x)+0.5*P1.x, 0.25*(P0.y+P2.y)+0.5*P1.y) ;
+            
+            //火箭在曲线最低点上方
+            CABasicAnimation * animation = [CABasicAnimation animation] ;
+            animation.keyPath = @"position" ;
+            animation.duration = 0.1f ;
+            animation.toValue = [NSValue valueWithCGPoint:CGPointMake(P.x, P.y - 32)] ;
+            [self.rockerLayer addAnimation:animation forKey:nil] ;
+            [self.rockerLayer setPosition:CGPointMake(P.x, P.y -32)] ;
+        }
+        
+        [path closePath] ;
+        self.shapeLayer.path = path.CGPath ;
     }
-    
-    [path closePath] ;
-    self.shapeLayer.path = path.CGPath ;
 }
+
+
 -(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
-    if(scrollView.contentOffset.y < -100){
-        [scrollView setContentOffset:CGPointMake(0, -100) animated:YES] ;
-    }else if(scrollView.contentOffset.y < 0){
-        [scrollView setContentOffset:CGPointMake(0, 0) animated:YES] ;
+    CGPoint currentCenterPoint = [self.scrollView.panGestureRecognizer locationInView:self.scrollView] ;
+    
+    //手指位置圆点
+    UIBezierPath * path = [UIBezierPath bezierPath] ;
+    [path moveToPoint:CGPointMake(currentCenterPoint.x, currentCenterPoint.y - 40)] ;
+    [path addArcWithCenter:currentCenterPoint radius:15.0 startAngle:- M_PI_2 endAngle:M_PI * 1.5 clockwise:YES] ;
+    self.circleLayer.path = path.CGPath ;
+    //取消抖动
+    [self.rockerLayer removeAnimationForKey:@"Jitter"] ;
+    
+    //火箭飞天
+    CABasicAnimation * fly = [CABasicAnimation animation] ;
+    fly.keyPath = @"position" ;
+    fly.toValue = [NSValue valueWithCGPoint:CGPointMake(self.view.center.x, -50)] ;
+    fly.duration = 0.06f ;
+    fly.removedOnCompletion = NO ;
+    fly.fillMode = kCAFillModeForwards ;
+    fly.delegate = self ;
+    [self.rockerLayer addAnimation:fly forKey:@"fly"] ;
+    [self.rockerLayer setPosition:CGPointMake(self.view.center.x, -50)] ;
+    self.rockerLayer = nil ;
+    
+    //手势圆圈fade
+    CABasicAnimation * animation = [CABasicAnimation animation] ;
+    animation.keyPath = @"fillColor" ;
+    animation.toValue = (id)[UIColor clearColor].CGColor ;
+    animation.duration = 1.5f ;
+    animation.removedOnCompletion = NO ;
+    animation.fillMode = kCAFillModeForwards ;
+    [self.circleLayer addAnimation:animation forKey:nil] ;
+    self.circleLayer = nil ;
+    
+    if(self.scrollView.contentOffset.y < -100){
+        [self.scrollView setContentOffset:CGPointMake(0, -100) animated:YES] ;
+    }else if(self. scrollView.contentOffset.y < 0){
+        [self.scrollView setContentOffset:CGPointMake(0, 0) animated:YES] ;
     }
 }
 -(void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView{
@@ -101,4 +176,51 @@
         
     };
 }
+
+//火箭晃动处理
+-(void)stateDetection{
+    static CGPoint lastPoint ;
+    if(!CGPointEqualToPoint(lastPoint, [self.scrollView.panGestureRecognizer locationInView:self.scrollView])){
+        lastPoint = [self.scrollView.panGestureRecognizer locationInView:self.scrollView] ;
+    }else if(self.scrollView.panGestureRecognizer.state == UIGestureRecognizerStateChanged){
+        if(self.timer){
+            [self rocketRotate] ;
+            [self.timer invalidate] ;
+            self.timer = nil ;
+        }
+    }
+}
+//-(void)drawCircle{
+//    CGPoint currentCenterPoint = [self.scrollView.panGestureRecognizer locationInView:self.scrollView] ;
+//
+//    UIBezierPath * path = [UIBezierPath bezierPath] ;
+//    [path moveToPoint:CGPointMake(currentCenterPoint.x, currentCenterPoint.y - 20)] ;
+//    [path addArcWithCenter:currentCenterPoint radius:15.0 startAngle:- M_PI_2 endAngle:M_PI * 1.5 clockwise:YES] ;
+//    self.circleLayer.path = path.CGPath ;
+//}
+-(void)rocketRotate{
+    //抖动
+        CAKeyframeAnimation * keyFram = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation.z"] ;
+        
+        //左晃偏移量
+        NSNumber * leftNumber = [NSNumber numberWithFloat:- 0.15*M_PI] ;
+        //原始位置
+        NSNumber * originNumber = [NSNumber numberWithFloat:0] ;
+        //右晃偏移量
+        NSNumber * rightNumber = [NSNumber numberWithFloat: 0.15*M_PI] ;
+        [keyFram setValues:@[originNumber, leftNumber, originNumber, rightNumber, originNumber]] ;
+        keyFram.repeatCount = MAXFLOAT ;
+        keyFram.repeatDuration = MAXFLOAT ;
+        [self.rockerLayer addAnimation:keyFram forKey:@"Jitter"] ;
+    
+}
+
+-(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag{
+    if([self.rockerLayer animationForKey:@"fly"] == anim){
+        NSLog(@"finish") ;
+        [self.rockerLayer removeAnimationForKey:@"fly"] ;
+        self.rockerLayer = nil ;
+    }
+}
+
 @end
